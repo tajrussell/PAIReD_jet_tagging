e88c95b25e69d335ad7e762b8947525e451418cf
Jan Schulz <83067174+JanGerritSchulz@users.noreply.github.com>
2024-07-17 00:46:33 +0200

    Update processDatasetToPAIReD.py

--- PAIReD_Data_Production/PFNano_to_PAIReD/src/processDatasetToPAIReD.py
+++ PAIReD_Data_Production/PFNano_to_PAIReD/src/processDatasetToPAIReD.py
@@ -3,6 +3,7 @@
 This script processes an entire data set from DAS to the PAIReD
 data format.
 """
+from dbs.apis.dbsClient import DbsApi
 from processFileToPAIReD import makeNtuplesPAIReDjointMC
 import argparse
 import multiprocessing as mp
@@ -10,20 +11,20 @@ import functools
 import os.path
 import os
 
-def process(storageprefix, datasetpath, filename, outputpath, batchsize, N_update,
-            N_events_to_process, physics_process, PAIReD_geometry, i=0):
+
+def process(storageprefix, outputpath, batchsize, N_update,
+            N_events_to_process, physics_process, PAIReD_geometry, inputfile=None):
     """
     Processes the file with index i.
 
     Args:
         i (int): index of the file to be processed
     """
-    # catch files named PFNANOAOD*_1-j.root by using indices i=1000+j instead
-    if i<1000:
-        PFNanopath = storageprefix + datasetpath + "/" + filename + "%i.root" % i
-    else:
-        PFNanopath = storageprefix + datasetpath + "/" + filename + "1-%i.root" % (i-1000)
-    PAIReDpath = outputpath + "/PAIReD_%i.root" % i
+    # path to PFNanoAOD file
+    PFNanopath = storageprefix + inputfile
+    # get index of the PFNanoAOD file and define the output file path
+    i = inputfile.rpartition("_")[-1][:-5]
+    PAIReDpath = outputpath + "/PAIReD_%s.root" % i
 
     # check if file even exists
     try:
@@ -35,13 +36,12 @@ def process(storageprefix, datasetpath, filename, outputpath, batchsize, N_updat
         return 0
 
 
-def processDataset(datasetpath,
+def processDataset(dataset,
                    outputpath,
                    storageprefix = "root://grid-cms-xrootd.physik.rwth-aachen.de/",
-                   filename = "PFNANOAODSIM_allPF_allBTV_",
                    batchsize = "100 MB",
-                   first_file = 1,
-                   N_files = 1,
+                   first_file = 0,
+                   N_files = -1,
                    N_update = 10,
                    N_events_to_process = -1,
                    N_threads = 1,
@@ -52,9 +52,21 @@ def processDataset(datasetpath,
     print("  Start processDataset()")
     print("#################################################################\n")
 
-    print("** Process data set stored in the following directory:")
-    print("   ", storageprefix + datasetpath)
+    print("** Process the following data set:")
+    print("   ", dataset)
+    print("** Storage prefix:", storageprefix)
+
+    # get list of files in the dataset
+    url="https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader"
+    api=DbsApi(url=url)
+    filelist = api.listFiles(dataset=dataset, detail=0)
+    filelist = [k['logical_file_name'] for k in filelist]
+
+    print("** Number of files in the data set:", len(filelist))
+    if N_files==-1:
+        N_files = len(filelist) - first_file
     print("** Number of files to be processed:", N_files)
+    print("** Start at file:", first_file)
 
     print("\nSettings for processFileToPAIReD:")
     print("** Batch size:", batchsize)
@@ -69,17 +81,18 @@ def processDataset(datasetpath,
         print("** Use only one thread")
         print("\n** Start processing the data set\n\n")
         for i in range(first_file, first_file + N_files):
-            process(storageprefix, datasetpath, filename, outputpath, batchsize, N_update,
-                        N_events_to_process, physics_process, PAIReD_geometry, i=i)
+            process(storageprefix, outputpath, batchsize, N_update,
+                        N_events_to_process, physics_process, PAIReD_geometry, 
+                        inputfile=filelist[i])
     # if run in parallel
     else:
-        process_for_pool = functools.partial(process, storageprefix, datasetpath,
-                                            filename, outputpath, batchsize, N_update,
+        process_for_pool = functools.partial(process, storageprefix,
+                                            outputpath, batchsize, N_update,
                                             N_events_to_process, physics_process, PAIReD_geometry)
         print("** Use %i threads in parallel" % N_threads)
         print("\n** Start processing the data set\n\n")
         with mp.Pool(processes=N_threads) as pool:
-            pool.map(process_for_pool, range(first_file, first_file + N_files))
+            pool.map(process_for_pool, filelist[first_file:(first_file + N_files)])
 
     print("\n\n** Done processing\n")
 
@@ -90,15 +103,14 @@ def processDataset(datasetpath,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process MC PFNano data set to the PAIReD data format for training.")
-    parser.add_argument("dataset", help="Path of the files of the data set to process (NanoAOD processed by PFNano, has to contain particle flow candidates (PFcands))")
+    parser.add_argument("dataset", help="Name of the data set (NanoAOD processed by PFNano, has to contain particle flow candidates (PFcands))")
     parser.add_argument("outputpath",  help="Path for the ROOT output files")
     parser.add_argument("-p", "--storageprefix", default="root://grid-cms-xrootd.physik.rwth-aachen.de/", help="Prefix of the storgae site the data set is stored on (default for T2_DE_RWTH site)")
-    parser.add_argument("-f", "--filename", default="PFNANOAODSIM_allPF_allBTV_", help="Default filename for files in data set without index number and root extension at the end (default is 'PFNANOAODSIM_allPF_allBTV_')")
     parser.add_argument("-b", "--batchsize", default="100 MB", help="Size of the batches of events that are handled simultaneously (default is '100 MB'). Can be an integer (=number of events) or a string (specifying the data size, e.g. '100 MB')")
     parser.add_argument("-u", "--N-update", type=int, default=10, help="Number of batches processed before printing an update in the terminal: e.g., 'process event batch 10 of 15' (default is 10)")
-    parser.add_argument("-n", "--N-files", type=int, default=1,  help="Number of files to be processed")
-    parser.add_argument("--start-file", type=int, default=1,  help="Index of first file to be processed (default is 1)")
-    parser.add_argument("-e", "--N-events", type=int, default=-1,  help="Number of events to be processed per file")
+    parser.add_argument("-n", "--N-files", type=int, default=-1,  help="Number of files to be processed. -1 means all (default is -1)")
+    parser.add_argument("--start-file", type=int, default=1,  help="Index of first file to be processed (default is 0)")
+    parser.add_argument("-e", "--N-events", type=int, default=-1,  help="Number of events to be processed per file. -1 means all (default is -1)")
     parser.add_argument("-t", "--N-threads", type=int, default=1,  help="Number of threads (default is 1)")
     parser.add_argument("--physicsprocess", type=int, default=0,  help="Integer indicating the physics process. Default is 0.")
     parser.add_argument("-g", "--geometry", type=str, default="Ellipse",  help="String indicating the geometry of the PAIReD jet. Default is Ellipse.")
@@ -106,7 +118,6 @@ if __name__ == "__main__":
 
     processDataset(args.dataset, args.outputpath,
                    storageprefix=args.storageprefix,
-                   filename=args.filename,
                    batchsize=args.batchsize,
                    first_file=args.start_file,
                    N_files=args.N_files,
