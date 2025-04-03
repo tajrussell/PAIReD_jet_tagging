d28971e0a9d777a6f4583bbb2dcd131fa7d14e11
trussell <trevor_russell@gmail.com>
2025-04-03 15:36:42 -0400

    editing ellipse and adding reweighting + new geometries

--- PAIReD_Data_Production/PFNano_to_PAIReD/src/fjcontrib-1.101/LundPlane/include/fastjet/contrib/LundPlane.hh
+++
@@ -1,9 +0,0 @@
-#ifndef __FASTJET_CONTRIB_LUNDPLANE_HH__
-#define __FASTJET_CONTRIB_LUNDPLANE_HH__
-
-// just include 
-#include "fastjet/contrib/LundGenerator.hh"
-#include "fastjet/contrib/LundWithSecondary.hh"
-#include "fastjet/contrib/RecursiveLundEEGenerator.hh"
-
-#endif // __FASTJET_CONTRIB_LUNDPLANE_HH__
